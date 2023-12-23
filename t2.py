3746e50c963c70f5f0d40fa336e86675725eb733
Lailiana Salsabila <144420632+Lailiana@users.noreply.github.com>
2023-12-23 22:38:21 +0700

    Add files via upload

---
+++ t2.py
@@ -0,0 +1,6 @@
+# Soal 3: Energi Kinetik
+massa_benda = 3  # massa benda dalam kg
+kecepatan_benda = 4  # kecepatan benda dalam m/s
+
+energi_kinetik = 0.5 * massa_benda * (kecepatan_benda ** 2)
+print("Energi Kinetik benda:", energi_kinetik, "Joule")
